eb010a8ebf652f68f7978c832c99681cd8c3de78
Mohit Udupa <moudupa@cisco.com>
2021-01-07 12:10:54 +0530

    Added generic list and dictionary types

---
+++ data.go
@@ -0,0 +1,7 @@
+package goutils
+
+// List is a generic type to hold list datatypes
+type List []interface{}
+
+// Dict is a generic type to hold dictionary datatypes
+type Dict map[string]interface{}
